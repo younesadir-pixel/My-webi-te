d5e4dc71337842950318441ad20d0eea7c88005b
sadiir <rhi99737@gmail.com>
2026-02-23 18:43:40 +0000

    Fix ESLint warnings in footer and add .env with CI=false

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => (
       <div className="footer-column">
         <h3 className="footer-heading">REJOINDRE FITNESS PARK GROUP</h3>
         <ul className="footer-list">
-          <li className="footer-list-item"><a className="footer-link" href="#">Proposer un local</a></li>
-          <li className="footer-list-item"><a className="footer-link" href="#">Recrutement</a></li>
-          <li className="footer-list-item"><a className="footer-link" href="#">Devenir franchisé</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">Proposer un local</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">Recrutement</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">Devenir franchisé</a></li>
         </ul>
       </div>
       <div className="footer-column">
         <h3 className="footer-heading">AIDE & INFORMATIONS</h3>
         <ul className="footer-list">
-          <li className="footer-list-item"><a className="footer-link" href="#">Abonnements</a></li>
-          <li className="footer-list-item"><a className="footer-link" href="#">Contact</a></li>
-          <li className="footer-list-item"><a className="footer-link" href="#">FAQ</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">Abonnements</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">Contact</a></li>
+          <li className="footer-list-item"><a className="footer-link" href="/">FAQ</a></li>
         </ul>
       </div>
       <div className="footer-column">
@@ -42,18 +42,18 @@ const Footer = () => (
       <div className="footer-logo-column">
         <div className="footer-brand-title">FITNESS PARK</div>
         <div className="footer-social-icons">
-          <a href="#" className="footer-social-link">FB</a>
-          <a href="#" className="footer-social-link">IG</a>
-          <a href="#" className="footer-social-link">TK</a>
+          <a href="/" className="footer-social-link">FB</a>
+          <a href="/" className="footer-social-link">IG</a>
+          <a href="/" className="footer-social-link">TK</a>
         </div>
       </div>
     </div>
     <div className="footer-bottom-bar">
-      <a className="footer-bottom-link" href="#">Mentions légales</a>
-      <a className="footer-bottom-link" href="#">CGV</a>
-      <a className="footer-bottom-link" href="#">CGU</a>
-      <a className="footer-bottom-link" href="#">Plan du site</a>
-      <a className="footer-bottom-link" href="#">Données Personnelles</a>
+      <a className="footer-bottom-link" href="/">Mentions légales</a>
+      <a className="footer-bottom-link" href="/">CGV</a>
+      <a className="footer-bottom-link" href="/">CGU</a>
+      <a className="footer-bottom-link" href="/">Plan du site</a>
+      <a className="footer-bottom-link" href="/">Données Personnelles</a>
     </div>
   </footer>
 );
