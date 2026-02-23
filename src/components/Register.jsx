31c1c2d5b3109bb643c98eaaaaa52780aaea1754
sadiir <rhi99737@gmail.com>
2026-02-23 18:29:07 +0000

    Fix remaining case-sensitivity issues in Auth.css imports

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
-import "./auth.css";
+import "./Auth.css";
 
 const Register = () => {
     const [formData, setFormData] = useState({
